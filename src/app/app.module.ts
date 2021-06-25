866da5077b564e8cc236cea3b8de5c777b1d75e8
Liosvel Medina Molina <liosvel.pepey@gmail.com>
2021-06-24 21:13:17 -0500

    create main-layout.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,23 @@ import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {MatButtonModule} from "@angular/material/button";
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
