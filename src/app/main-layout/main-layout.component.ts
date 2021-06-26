b29fd6ad6affd648b86bf283d395d45ed1225172
Liosvel Medina Molina <liosvel.pepey@gmail.com>
2021-06-26 09:17:56 -0500

    customize sidenav

--- src/app/main-layout/main-layout.component.ts
+++ src/app/main-layout/main-layout.component.ts
@@ -9,21 +9,27 @@ export class MainLayoutComponent implements OnInit {
   links = [
     {type: 'item', path: 'home', icon: 'home', label: 'Inicio'},
     {type: 'item', path: 'events', icon: 'date_range', label: 'Eventos'},
-    {type: 'item', path: 'praying-calendar', icon: 'description', label: 'Calendario de oración'},
+    {type: 'item', path: 'praying-calendar', icon: 'notes', label: 'Calendario de oración'},
     {type: 'item', path: 'anniversaries', icon: 'cake', label: 'Aniversarios'},
-    {type: 'item', path: 'directory', icon: 'person', label: 'Directorio'},
+    {type: 'item', path: 'directory', icon: 'account_box', label: 'Directorio'},
     {type: 'divider'},
     {type: 'item', path: 'settings', icon: 'settings', label: 'Configuración'},
     {type: 'divider'},
-    {type: 'item', path: 'terms', icon: 'description', label: 'Términos y condiciones'},
+    {type: 'item', path: 'terms', icon: 'insert_drive_file', label: 'Términos y condiciones'},
     {type: 'item', path: 'about', icon: 'info', label: 'Acerca de Proyecciones'},
-    {type: 'item', path: 'donate', icon: 'money', label: 'Donar'},
+    {type: 'item', path: 'donate', icon: 'paid', label: 'Donar'},
   ];
+
+  sideMenuVisible = true;
 
   constructor() {
   }
 
   ngOnInit(): void {
+  }
+
+  toggleSideMenu() {
+    this.sideMenuVisible = !this.sideMenuVisible
   }
 
 }
