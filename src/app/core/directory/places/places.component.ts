5070f44934eb5fd26b211f9ae0c2dd71f9ffa8e4
Liosvel Medina Molina <liosvel.pepey@gmail.com>
2021-07-04 14:29:37 -0500

    define places.component stricture

--- src/app/core/directory/places/places.component.ts
+++ src/app/core/directory/places/places.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+
+export interface Place {
+  name: string;
+  address?: string;
+}
 
 @Component({
   selector: 'app-places',
@@ -7,9 +12,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlacesComponent implements OnInit {
 
-  constructor() { }
+  scrollTop = 0;
+
+  places: Place[] = [
+    {
+      name: 'Campamento Bautista',
+      address: 'Carretera a lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa ipsam magnam odio placeat rerum. Dolores doloribus maxime neque nesciunt nisi odit perspiciatis similique veniam! Adipisci in maiores mollitia placeat veritatis.'
+    },
+    {
+      name: 'Centro Bautista',
+      address: 'Carretera a lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa ipsam magnam odio placeat rerum. Dolores doloribus maxime neque nesciunt nisi odit perspiciatis similique veniam! Adipisci in maiores mollitia placeat veritatis.'
+    },
+    {
+      name: 'Hogar de Ancianos Bautista',
+      address: 'Carretera a lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa ipsam magnam odio placeat rerum. Dolores doloribus maxime neque nesciunt nisi odit perspiciatis similique veniam! Adipisci in maiores mollitia placeat veritatis.'
+    },
+    {
+      name: 'Seminario Bautista',
+      address: 'Carretera a lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa ipsam magnam odio placeat rerum. Dolores doloribus maxime neque nesciunt nisi odit perspiciatis similique veniam! Adipisci in maiores mollitia placeat veritatis.'
+    },
+    {
+      name: 'Campamento Bautista',
+      address: 'Carretera a lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa ipsam magnam odio placeat rerum. Dolores doloribus maxime neque nesciunt nisi odit perspiciatis similique veniam! Adipisci in maiores mollitia placeat veritatis.'
+    },
+    {
+      name: 'Centro Bautista',
+      address: 'Carretera a lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa ipsam magnam odio placeat rerum. Dolores doloribus maxime neque nesciunt nisi odit perspiciatis similique veniam! Adipisci in maiores mollitia placeat veritatis.'
+    },
+    {
+      name: 'Hogar de Ancianos Bautista',
+      address: 'Carretera a lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa ipsam magnam odio placeat rerum. Dolores doloribus maxime neque nesciunt nisi odit perspiciatis similique veniam! Adipisci in maiores mollitia placeat veritatis.'
+    },
+    {
+      name: 'Seminario Bautista',
+      address: 'Carretera a lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa ipsam magnam odio placeat rerum. Dolores doloribus maxime neque nesciunt nisi odit perspiciatis similique veniam! Adipisci in maiores mollitia placeat veritatis.'
+    },
+  ];
+
+  @ViewChild('panel') panel: ElementRef;
+
+  constructor(panel: ElementRef) {
+    this.panel = panel;
+  }
 
   ngOnInit(): void {
+  }
+
+  scrollPanelToTop() {
+    if (this.panel != undefined) {
+      this.panel.nativeElement.scrollTop = 0;
+    }
+  }
+
+  onScroll(event: any) {
+    this.scrollTop = event.target.scrollTop;
+  }
+
+  isGoUpVisible() {
+    return this.scrollTop > 48;
   }
 
 }
