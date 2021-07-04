8b4eff49e9cc3fc793f7ed34128317374e900a08
Liosvel Medina Molina <liosvel.pepey@gmail.com>
2021-07-04 10:26:22 -0500

    redirect /directory to /directory/churches on navigation routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -32,6 +32,7 @@ const routes: Routes = [
           {path: 'churches', component: ChurchesComponent},
           {path: 'pastors', component: PastorsComponent},
           {path: 'places', component: PlacesComponent},
+          {path: '', redirectTo: 'churches', pathMatch: 'full'}
         ]
       },
 
