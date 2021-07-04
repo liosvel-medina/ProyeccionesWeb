cc0615d949cb5d040b56e91a784e397533ffdf12
Liosvel Medina Molina <liosvel.pepey@gmail.com>
2021-07-04 14:21:39 -0500

    define pastors.component structure

--- src/app/core/directory/pastors/pastors.component.ts
+++ src/app/core/directory/pastors/pastors.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+
+export interface Pastor {
+  name: string;
+  place?: string;
+}
 
 @Component({
   selector: 'app-pastors',
@@ -7,9 +12,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PastorsComponent implements OnInit {
 
-  constructor() { }
+  scrollTop = 0;
+
+  pastors: Pastor[] = [
+    {name: 'Abdiel Morfa Cabrera', place: 'Seminario Teológico Bautista Dr. Rafael'},
+    {name: 'Abel Pérez Hernámdez', place: 'Iglesia Bautista Alcance Victoria'},
+    {name: 'Adolfo Díaz Acosta', place: 'Iglesia Bautista Reparto Oriente'},
+    {name: 'Adonis Alonso Arteaga', place: 'Iglesia Bautista Bayate'},
+    {name: 'Adriel Carrazana Monteagudo', place: 'Iglesia Bautista Los Pinos'},
+    {name: 'Adán Barrera Álvarez', place: 'Iglesia Bautista Alonso Rojas'},
+    {name: 'Abel Pérez Hernámdez', place: 'Iglesia Bautista Alcance Victoria'},
+    {name: 'Adolfo Díaz Acosta', place: 'Iglesia Bautista Reparto Oriente'},
+    {name: 'Adonis Alonso Arteaga', place: 'Iglesia Bautista Bayate'},
+    {name: 'Adriel Carrazana Monteagudo', place: 'Iglesia Bautista Los Pinos'},
+    {name: 'Adán Barrera Álvarez', place: 'Iglesia Bautista Alonso Rojas'},
+    {name: 'Abel Pérez Hernámdez', place: 'Iglesia Bautista Alcance Victoria'},
+    {name: 'Adolfo Díaz Acosta', place: 'Iglesia Bautista Reparto Oriente'},
+    {name: 'Adonis Alonso Arteaga', place: 'Iglesia Bautista Bayate'},
+    {name: 'Adriel Carrazana Monteagudo', place: 'Iglesia Bautista Los Pinos'},
+    {name: 'Adán Barrera Álvarez', place: 'Iglesia Bautista Alonso Rojas'},
+    {name: 'Abel Pérez Hernámdez', place: 'Iglesia Bautista Alcance Victoria'},
+    {name: 'Adolfo Díaz Acosta', place: 'Iglesia Bautista Reparto Oriente'},
+    {name: 'Adonis Alonso Arteaga', place: 'Iglesia Bautista Bayate'},
+    {name: 'Adriel Carrazana Monteagudo', place: 'Iglesia Bautista Los Pinos'},
+    {name: 'Adán Barrera Álvarez', place: 'Iglesia Bautista Alonso Rojas'},
+    {name: 'Abel Pérez Hernámdez', place: 'Iglesia Bautista Alcance Victoria'},
+    {name: 'Adolfo Díaz Acosta', place: 'Iglesia Bautista Reparto Oriente'},
+    {name: 'Adonis Alonso Arteaga', place: 'Iglesia Bautista Bayate'},
+    {name: 'Adriel Carrazana Monteagudo', place: 'Iglesia Bautista Los Pinos'},
+    {name: 'Adán Barrera Álvarez', place: 'Iglesia Bautista Alonso Rojas'},
+    {name: 'Abel Pérez Hernámdez', place: 'Iglesia Bautista Alcance Victoria'},
+    {name: 'Adolfo Díaz Acosta', place: 'Iglesia Bautista Reparto Oriente'},
+    {name: 'Adonis Alonso Arteaga', place: 'Iglesia Bautista Bayate'},
+    {name: 'Adriel Carrazana Monteagudo', place: 'Iglesia Bautista Los Pinos'},
+    {name: 'Adán Barrera Álvarez', place: 'Iglesia Bautista Alonso Rojas'},
+  ];
+
+  @ViewChild('panel') panel: ElementRef;
+
+  constructor(panel: ElementRef) {
+    this.panel = panel;
+  }
 
   ngOnInit(): void {
+  }
+
+  scrollPanelToTop() {
+    if (this.panel != undefined) {
+      this.panel.nativeElement.scrollTop = 0;
+    }
+  }
+
+  onScroll(event: any) {
+    this.scrollTop = event.target.scrollTop;
+  }
+
+  isGoUpVisible() {
+    return this.scrollTop > 48;
   }
 
 }
